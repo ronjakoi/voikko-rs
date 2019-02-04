07ed3ef9923d93440040e6a0e5d9e05e4d00ac1c
Ronja Koistinen <ronja.koistinen@kapsi.fi>
2019-02-04 16:07:21 +0200

    Doc comments and a typo in function name

--- src/lib.rs
+++ src/lib.rs
@@ -369,68 +369,138 @@ pub mod voikko {
 
         // Boolean options
 
+        /// Ignore dot at the end of the word (needed for use in some word processors).
+        /// If this option is set and input word ends with a dot, spell checking and
+        /// hyphenation functions try to analyze the word without the dot if no results
+        /// can be obtained for the original form. Also with this option, string tokenizer
+        /// will consider trailing dot of a word to be a part of that word.
+        ///
+        /// Default: false
         pub fn set_opt_ignore_dot(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 0, value)
         }
 
+        /// (Spell checking only) Ignore words containing numbers
+        ///
+        /// Default: false
         pub fn set_opt_ignore_numbers(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 1, value)
         }
 
+        /// Accept words that are written completely in uppercase letters without checking
+        /// them at all.
+        ///
+        /// Default: false
         pub fn set_opt_ignore_uppercase(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 3, value)
         }
 
+        /// Accept words even when the first letter is in uppercase (start of sentence etc.)
+        ///
+        /// Default: true
         pub fn set_opt_accept_first_uppercase(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 6, value)
         }
 
+        /// Accept words even when all of the letters are in uppercase. Note that this is
+        /// not the same as `set_opt_ignore_uppercase(true)`: with this option the word is still
+        /// checked, only case differences are ignored.
+        ///
+        /// Default: true
         pub fn set_opt_accept_all_uppercase(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 7, value)
         }
 
+        /// Do not insert hyphenation positions that are considered to be ugly but correct
+        ///
+        /// Default: false
         pub fn set_opt_no_ugly_hyphenation(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 4, value)
         }
 
+        /// Use suggestions optimized for optical character recognition software.
+        /// By default suggestions are optimized for typing errors.
+        ///
+        /// Default: false
         pub fn set_opt_ocr_suggestions(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 8, value)
         }
 
+        /// (Spell checking only): Ignore non-words such as URLs and email addresses.
+        ///
+        /// Default: true
         pub fn set_opt_ignore_nonwords(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 10, value)
         }
 
+        /// (Spell checking only): Allow some extra hyphens in words. This option relaxes
+        /// hyphen checking rules to work around some unresolved issues in the underlying
+        /// morphology, but it may cause some incorrect words to be accepted. The exact
+        /// behavior (if any) of this option is not specified.
+        ///
+        /// Default: false */
         pub fn set_opt_accept_extra_hyphens(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 11, value)
         }
 
+        /// (Spell checking only): Accept missing hyphens at the start and end of the word.
+        /// Some application programs do not consider hyphens to be word characters. This
+        /// is a reasonable assumption for many languages but not for Finnish. If the
+        /// application cannot be fixed to use a proper tokenisation algorithm for Finnish,
+        /// this option may be used to tell libvoikko to work around this defect.
+        ///
+        /// Default: false
         pub fn set_opt_accept_missing_hyphens(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 12, value)
         }
 
+        /// (Grammar checking only): Accept incomplete sentences that could occur in
+        /// titles or headings. Set this option to true if your application is not able
+        /// to differentiate titles from normal text paragraphs, or if you know that
+        /// you are checking title text.
+        ///
+        /// Default: false
         pub fn set_opt_accept_titles_in_gc(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 13, value)
         }
 
-        pub fn set_opt_accept_unfinished_paragrahs_in_gc(&self, value: bool) -> bool {
+        /// (Grammar checking only): Accept incomplete sentences at the end of the
+        /// paragraph. These may exist when text is still being written.
+        ///
+        /// Default: false
+        pub fn set_opt_accept_unfinished_paragraphs_in_gc(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 14, value)
         }
 
+        /// (Hyphenation only): Hyphenate unknown words.
+        ///
+        /// Default: true
         pub fn set_opt_hyphenate_unknown_words(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 15, value)
         }
 
+        /// (Grammar checking only): Accept paragraphs if they would be valid within
+        /// bulleted lists.
+        ///
+        /// Default: false
         pub fn set_opt_accept_bulleted_lists_in_gc(&self, value: bool) -> bool {
             libvoikko::set_bool_option(self.handle, 16, value)
         }
 
         // Integer options
 
+        /// The minimum length for words that may be hyphenated. This limit is also enforced on
+        /// individual parts of compound words.
+        ///
+        /// Default: 2
         pub fn set_min_hyphenated_word_length(&self, value: isize) -> bool {
             libvoikko::set_int_option(self.handle, 9, value)
         }
 
+        /// Size of the spell checker cache. This can be -1 (no cache) or
+        /// >= 0 ( size in bytes = `2^cache_size * (6544*sizeof(wchar_t) + 1008)` ).
+        ///
+        /// Default: 0
         pub fn set_speller_cache_size(&self, value: isize) -> bool {
             libvoikko::set_int_option(self.handle, 17, value)
         }

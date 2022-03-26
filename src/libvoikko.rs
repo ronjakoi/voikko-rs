446e9a4f21df240767bdc15260f5ace4bfa55693
Ronja Koistinen <ronja.koistinen@kapsi.fi>
2022-03-26 22:12:30 +0200

    Update copyrigh dates

--- src/libvoikko.rs
+++ src/libvoikko.rs
@@ -1,5 +1,5 @@
 /*  voikko-rs - libvoikko bindings for the Rust programming language
-    Copyright (C) 2019-2021 Ronja Koistinen
+    Copyright (C) 2019-2022 Ronja Koistinen
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
